e66b6d10c71e34ab73aad9ed0539e90788e7e875
Gundersond <dalton.gunderson@hotmail.com>
2020-10-18 08:30:09 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+    def initialize(school)
+        @roster = {}
+        @school = school
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.include?(grade)
+            @roster[grade] << "#{name}"
+        else
+            @roster[grade] = []
+            @roster[grade] << "#{name}"
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.transform_values {|student| student.sort}
+    end
+
+end
